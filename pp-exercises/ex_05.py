472d3733b684397fd0fe18a54e8d46d01cf447a3
Skylar Iolta <siolta@users.noreply.github.com>
2018-11-28 14:52:04 -0800

    Added new excercise

---
+++ pp-exercises/ex_05.py
@@ -0,0 +1,15 @@
+
+a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
+b = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+
+new_list = []
+
+for i in a:
+    if i not in new_list:
+        new_list.append(i)
+
+for i in b:
+    if i not in new_list:
+        new_list.append(i)
+
+print(new_list)
