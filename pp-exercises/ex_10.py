1d465f80a82d460398bc66565c81e8a22911f7a2
Skylar Iolta <siolta@users.noreply.github.com>
2018-11-28 15:20:13 -0800

    fixed type error in ex 10

--- pp-exercises/ex_10.py
+++ pp-exercises/ex_10.py
@@ -1,5 +1,7 @@
-a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
-b = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+import random
+
+a = [random.randint(0, 50) for i in range(random.randint(0,20))]
+b = [random.randint(0, 50) for i in range(random.randint(0,20))]
 
 
 def commons(a, b):
@@ -8,7 +10,7 @@ def commons(a, b):
     c = []
     which_min = a if len(a) < len(b) else b
 
-    for i in range(which_min):
+    for i in range(len(which_min)):
         if a[a_I] > b[b_I]:
             c.append(a[a_I])
             a_I += 1
@@ -16,6 +18,8 @@ def commons(a, b):
             c.append(b[b_I])
             b_I += 1
         elif a[a_I] == b[b_I]:
+            if a[a_I] not in c:
+                c.append(a[a_I])
             if len(a) < len(b):
                 a_I += 1
             else:
