7bae078a306d0203c2cd3fb4070852ed57d66bbc
Skylar Iolta <siolta@users.noreply.github.com>
2018-11-28 15:19:58 -0800

    python excercise 05

--- pp-exercises/ex_05.py
+++ pp-exercises/ex_05.py
@@ -1,8 +1,12 @@
+import random
 
-a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
-b = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+a = [random.randint(0, 50) for i in range(random.randint(0,20))]
+b = [random.randint(0, 50) for i in range(random.randint(0,20))]
 
 new_list = []
+
+print(f"List a: {a}")
+print(f"List b: {b}")
 
 for i in a:
     if i not in new_list:
@@ -12,4 +16,4 @@ for i in b:
     if i not in new_list:
         new_list.append(i)
 
-print(new_list)
+print(sorted(new_list))
