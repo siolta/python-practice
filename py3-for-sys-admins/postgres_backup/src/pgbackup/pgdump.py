a6139f565f695112a5303b41882847b1f8a0824c
Skylar Iolta <siolta@users.noreply.github.com>
2019-05-15 16:00:41 -0700

    framework for psql backups

---
+++ py3-for-sys-admins/postgres_backup/src/pgbackup/pgdump.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+import subprocess
+import sys
+
+def dump(url):
+    try:
+        return subprocess.Popen(['pg_dump', url], stdout=subprocess.PIPE)
+    except OSError as err:
+        print(f"Error: {err}")
+        sys.exit(1)
---
+++ py3-for-sys-admins/postgres_backup/src/pgbackup/storage.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+def local(infile, outfile):
+    outfile.write(infile.read())
+    outfile.close()
+    infile.close()
+
+def s3(client, infile, bucket, filename):
+    client.upload_fileobj(infile, bucket, filename)
+
