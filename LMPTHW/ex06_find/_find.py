7629546afc09b4179dabb9b8c0d78195e1a41dda
Skylar Iolta <siolta@users.noreply.github.com>
2019-08-20 16:33:29 -0700

    rewrite _find to use pathlib.Path

--- LMPTHW/ex06_find/_find.py
+++ LMPTHW/ex06_find/_find.py
@@ -3,44 +3,49 @@
 import argparse
 import os
 import subprocess
-import glob # rewrite to use Path from pathlib?
+import sys
+from pathlib import Path
 
 def parse_args():
 
     parser = argparse.ArgumentParser()
 
-    parser.add_argument('-d', '--directory', default='.', help='directory to search in')
+    parser.add_argument('dir', nargs=1, help='directory to search in')
     parser.add_argument('-n', '--name', help='name of file or directory to look for')
-    parser.add_argument('-t', '--type', choices=['file', 'directory'], default='file', help='type of file: executable, or directory')
+    parser.add_argument('-t', '--type', choices=['file', 'directory'], help='type of file: executable, or directory')
     parser.add_argument('-e', '--exec', help='command to run on each file')
-    parser.add_argument('-p', '--print', action='store_true', help='print out each file')
 
     return parser.parse_args()
 
-def find_files(args):
-    return glob.glob(f'{args.directory}/{args.name}')
+def find_by_name(_dir, args):
+    for f in _dir.rglob(args.name):
+        print(f)
 
-def find_directories(args):
-    return glob.glob(f'{args.directory}/**/')
+def find_by_type(_dir, args):
+    if args.type not in ['directory', 'file']:
+        print(f'Unknown type: {args.type}')
+        sys.exit(1)
+
+    for f in Path(f'{_dir}').rglob(args.name or '*'):
+        if args.type == 'file' and f.is_file():
+            print(f)
+        elif args.type == 'directory' and f.is_dir():
+            print(f)
 
 def run_exec(args):
     _exec = subprocess.run([f'{args.exec}'], capture_output=True)
     return _exec.stdout.splitlines()
 
-def run():
-    args = parse_args()
-    if args.type == 'file':
-        items = find_files(args)
-    elif args.type == 'directory':
-        items = find_directories(args)
+def find_the_things(args):
+    _dir = Path(args.dir[0])
 
-    for item in items:
-        if args.print:
-            print(item)
-        if args.exec is not None:
-            output = run_exec(args)
-            for line in output:
-                print(line.decode("utf-8"))
+    if args.name and not args.type:
+        find_by_name(_dir, args)
+    elif args.type:
+        find_by_type(_dir, args)
+    else:
+        print("You need either --name or --type")
+        sys.exit(1)    
 
 
-run()
+find_the_things(parse_args())
