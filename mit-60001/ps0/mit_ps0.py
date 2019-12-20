308efb96121e65281a2cf2dc4a617989bf6dd39a
Skylar.Iolta <siolta@users.noreply.github.com>
2019-12-20 08:04:58 -0800

    fix some nooby mistakes

--- mit-60001/ps0/mit_ps0.py
+++ mit-60001/ps0/mit_ps0.py
@@ -2,9 +2,9 @@
 
 import numpy
 
-x = input("Enter number x: ")
-y = input("Enter number y: ")
+x = int(input("Enter number x: "))
+y = int(input("Enter number y: "))
 
 print(f"x**y = {x**y}")
 
-print(f"log(x) = {log(x)}")
+print(f"log(x) = {numpy.log(x)}")
