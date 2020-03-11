6eaf0ff31ffef4770b7b4015e937f4b4654c123f
Skylar Iolta <skylar.iolta@gmail.com>
2020-03-10 19:46:48 -0700

    initial cut version

--- LMPTHW/ex08_cut/_cut.py
+++ LMPTHW/ex08_cut/_cut.py
@@ -1,3 +1,21 @@
 #!/usr/bin/env python3
 
 import argparse
+
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('file')
+    parser.add_argument('-d', dest='delim')
+
+    return parser.parse_args()
+
+args = parse_args()
+
+with open(args.file) as f:
+    for line in f.readlines():
+        print(line.split(args.delim))
+    
+
+print(args)
