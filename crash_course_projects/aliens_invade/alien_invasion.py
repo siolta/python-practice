eb63a7d71ca6bc85f4f40caa40704b810c88e365
Skylar Iolta <23390416+siolta@users.noreply.github.com>
2024-05-30 23:41:10 -0700

    added ship to screen

--- crash_course_projects/aliens_invade/alien_invasion.py
+++ crash_course_projects/aliens_invade/alien_invasion.py
@@ -3,6 +3,7 @@ import sys
 import pygame
 
 from settings import Settings
+from ship import Ship
 
 
 class AlienInvasion:
@@ -14,9 +15,10 @@ class AlienInvasion:
         self.clock = pygame.time.Clock()
         self.settings = Settings()
 
-        self.screen = pygame.display.set_mode(
-            self.settings.screen_width, self.settings.screen_height)
+        self.screen = pygame.display.set_mode((
+            self.settings.screen_width, self.settings.screen_height))
         pygame.display.set_caption("Alien Invasion")
+        self.ship = Ship(self)
 
         # Set the background color
         self.bg_color = (self.settings.bg_color)
@@ -31,6 +33,7 @@ class AlienInvasion:
 
             # Redraw the screen during each pass through the loop
             self.screen.fill(self.bg_color)
+            self.ship.blitme()
 
             # Make the most recently drawn screen visible.
             pygame.display.flip()
