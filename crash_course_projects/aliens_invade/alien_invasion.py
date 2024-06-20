68e7bd2a32b2f16809d209a955f86f65fa11a427
Skylar Iolta <23390416+siolta@users.noreply.github.com>
2024-06-20 00:22:37 -0700

    ship and invader movement, key debug output

--- crash_course_projects/aliens_invade/alien_invasion.py
+++ crash_course_projects/aliens_invade/alien_invasion.py
@@ -17,6 +17,10 @@ class AlienInvasion:
 
         self.screen = pygame.display.set_mode((
             self.settings.screen_width, self.settings.screen_height))
+        # Settings for fullscreen:
+        # self.screen = pygame.display.set_mode((0,0), pygame.FULLSCREEN)
+        # self.settings.screen_width = self.screen.get_rect().width
+        # self.settings.screen_height = self.screen.get_rect().height
         pygame.display.set_caption("Alien Invasion")
         self.ship = Ship(self)
 
@@ -27,6 +31,7 @@ class AlienInvasion:
         """Start the main loop for the game."""
         while True:
             self._check_events()
+            self.ship.update()
             self._update_screen()
             self.clock.tick(60)
 
@@ -35,6 +40,28 @@ class AlienInvasion:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
+            elif event.type == pygame.KEYDOWN:
+                self._check_keydown_events(event)
+            elif event.type == pygame.KEYUP:
+                self._check_keyup_events(event)
+
+    def _check_keydown_events(self, event):
+        """Respond to key presses."""
+        if event.key == pygame.K_RIGHT:
+            # Move the ship to the right
+            self.ship.moving_right = True
+        if event.key == pygame.K_LEFT:
+            # Move the ship to the left
+            self.ship.moving_left = True
+        elif event.key == pygame.K_q:
+            sys.exit()
+
+    def _check_keyup_events(self, event):
+        """Respond to key releases."""
+        if event.key == pygame.K_RIGHT:
+            self.ship.moving_right = False
+        if event.key == pygame.K_LEFT:
+            self.ship.moving_left = False
 
     def _update_screen(self):
         """Update images on the screen, and flip to the new screen."""
--- crash_course_projects/aliens_invade/try_it/try_it.py
+++ crash_course_projects/aliens_invade/try_it/try_it.py
@@ -14,7 +14,10 @@ class TryIt:
         self.clock = pygame.time.Clock()
         self.screen_width = 1200
         self.screen_height = 800
-        self.bg_color = (49, 55, 139)
+        self.bg_color = (50, 55, 140)
+        self.GAME_FONT = pygame.freetype.SysFont('Mono', 18)
+        self.DEBUG = False
+        self._key_press_debug = pygame.Surface((0, 0))
 
         self.screen = pygame.display.set_mode((self.screen_width,
                                                self.screen_height))
@@ -28,6 +31,7 @@ class TryIt:
         """start game loop"""
         while True:
             self._check_events()
+            self.character.update()
             self._update_screen()
             self.clock.tick(60)
 
@@ -37,10 +41,47 @@ class TryIt:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
+            elif event.type == pygame.KEYDOWN:
+                if self.DEBUG:
+                    self._debug_keys(event)
+                self._check_keydown_events(event)
+            elif event.type == pygame.KEYUP:
+                self._check_keyup_events(event)
+
+    def _debug_keys(self, event):
+        # print(event.key)
+        self._key_press_debug, _ = self.GAME_FONT.render(
+            str(event.key), (0, 0, 0))
+
+    def _check_keydown_events(self, event):
+        """respond to keypresses"""
+        if event.key == pygame.K_RIGHT:
+            self.character.moving_right = True
+        if event.key == pygame.K_LEFT:
+            self.character.moving_left = True
+        if event.key == pygame.K_UP:
+            self.character.moving_up = True
+        if event.key == pygame.K_DOWN:
+            self.character.moving_down = True
+
+    def _check_keyup_events(self, event):
+        """respond to keypresses"""
+        if event.key == pygame.K_RIGHT:
+            self.character.moving_right = False
+        if event.key == pygame.K_LEFT:
+            self.character.moving_left = False
+        if event.key == pygame.K_UP:
+            self.character.moving_up = False
+        if event.key == pygame.K_DOWN:
+            self.character.moving_down = False
 
     def _update_screen(self):
         """Update images on the screen, and flip to the new screen."""
         self.screen.fill(self.bg_color)
+        # print key inputs if DEBUG
+        if self.DEBUG:
+            self.screen.blit(self._key_press_debug,
+                             self.screen.get_rect().midleft)
         self.character.blitme()
 
         pygame.display.flip()
@@ -54,6 +95,12 @@ class Character:
         self.screen = try_it_game.screen
         self.screen_rect = try_it_game.screen.get_rect()
 
+        # movement flags
+        self.moving_right = False
+        self.moving_left = False
+        self.moving_up = False
+        self.moving_down = False
+
         # load character image and get rect
         self.image = pygame.image.load('images/invader.png')
         self.rect = self.image.get_rect()
@@ -61,9 +108,31 @@ class Character:
         # start at center of screen
         self.rect.center = self.screen_rect.center
 
+        # set speed value
+        self.char_speed = 2.5
+
+        # store float for characters's exact position
+        self.x = float(self.rect.x)
+        self.y = float(self.rect.y)
+
     def blitme(self):
         """draw character at current location."""
         self.screen.blit(self.image, self.rect)
+
+    def update(self):
+        """Update the characters's position based on movement flags."""
+        if self.moving_right and self.rect.right < self.screen_rect.right:
+            self.x += self.char_speed
+        if self.moving_left and self.rect.left > 0:
+            self.x -= self.char_speed
+        if self.moving_up and self.rect.top > 0:
+            self.y -= self.char_speed
+        if self.moving_down and self.rect.bottom < self.screen_rect.bottom:
+            self.y += self.char_speed
+
+        # update char rect object
+        self.rect.x = self.x
+        self.rect.y = self.y
 
 
 if __name__ == '__main__':
