6ddb4dc83ea9dddee444df7c15a9eecbeeb68559
Skylar Iolta <23390416+siolta@users.noreply.github.com>
2024-12-19 06:34:51 -0800

    add initial hangman game

---
+++ self_taught/hangman.py
@@ -0,0 +1,34 @@
+def hangman(word):
+    wrong = 0
+    stages = ["",
+              "_______          ",
+              "|                ",
+              "|       |        ",
+              "|       O        ",
+              "|      /|\       ",
+              "|      / \       ",
+              "|                "
+              ]
+    rletters = list(word)
+    board = ["___"] * len(word)
+    win = False
+    print("Welcome to Hangman")
+
+    while wrong < len(stages) - 1:
+        print("\n")
+        msg = "Guessa a letter"
+        char = input(msg)
+        if char in rletters:
+            cind = rletters.index(char)
+            board[cind] = char
+            rletters[cind] = '$'
+        else:
+            wrong += 1
+        print((" ".join(board)))
+        e = wrong + 1
+        print("\n".join(stages[0: e]))
+        if "___" not in board:
+            print("You win!")
+            print(" ".join(board))
+            win = True
+            break
