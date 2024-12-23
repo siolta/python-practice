40654537f8140901f0da905ce00d4b030ca5261a
Skylar Iolta <23390416+siolta@users.noreply.github.com>
2024-12-23 00:46:04 -0800

    finished hangman

--- self_taught/hangman.py
+++ self_taught/hangman.py
@@ -1,34 +1,46 @@
+from random import randrange
+
+
 def hangman(word):
     wrong = 0
     stages = ["",
-              "_______          ",
+              "|________________",
               "|                ",
               "|       |        ",
               "|       O        ",
-              "|      /|\       ",
-              "|      / \       ",
+              "|      /|\\       ",
+              "|      / \\       ",
               "|                "
               ]
     rletters = list(word)
-    board = ["___"] * len(word)
+    board = ["_"] * len(word)
     win = False
     print("Welcome to Hangman")
 
     while wrong < len(stages) - 1:
         print("\n")
-        msg = "Guessa a letter"
+        msg = "Guess a letter: \n"
         char = input(msg)
         if char in rletters:
             cind = rletters.index(char)
             board[cind] = char
-            rletters[cind] = '$'
+            rletters[cind] = "$"
         else:
             wrong += 1
+        print(board)
         print((" ".join(board)))
         e = wrong + 1
         print("\n".join(stages[0: e]))
-        if "___" not in board:
+        if "_" not in board:
             print("You win!")
             print(" ".join(board))
             win = True
             break
+    if not win:
+        print("\n".join(stages[0: wrong]))
+        print("You lose! It was {}.".format(word))
+
+
+words = ["cat", "dog", "house", "fortune"]
+
+hangman(words[randrange(len(words))])
