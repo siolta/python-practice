b0056e65d3f6898b69998957a445622ef3ac9ae3
Skylar Iolta <23390416+siolta@users.noreply.github.com>
2025-02-25 15:29:44 -0800

    add initial asteroids loop

---
+++ asteroids/main.py
@@ -0,0 +1,28 @@
+# this allows us to use code from
+# the open-source pygame library
+# throughout this file
+import pygame
+from constants import *
+
+
+def main():
+    print("Starting Asteroids!")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    while True:
+        update_screen(screen)
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+
+
+def update_screen(screen):
+    black = (0, 0, 0)
+    screen.fill(black)
+    pygame.display.flip()
+
+
+if __name__ == "__main__":
+    main()
