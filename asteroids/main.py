1a36438fa2213fa6fc0ccd0c7e4cca7ae612ff25
Skylar Iolta <23390416+siolta@users.noreply.github.com>
2025-02-25 16:26:55 -0800

    fill screen, limit FPS

--- asteroids/main.py
+++ asteroids/main.py
@@ -6,21 +6,24 @@ from constants import *
 
 
 def main():
-    print("Starting Asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
+
     while True:
-        update_screen(screen)
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
 
+        update_screen(screen)
+
+        # limit framerate to 60fps
+        dt = clock.tick(60) / 1000
+
 
 def update_screen(screen):
-    black = (0, 0, 0)
-    screen.fill(black)
+    screen.fill("black")
     pygame.display.flip()
 
 
