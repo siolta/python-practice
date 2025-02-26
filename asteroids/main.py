e41d5451e06ff51db62f5651892dbaed3add0d69
Skylar Iolta <23390416+siolta@users.noreply.github.com>
2025-02-26 00:01:14 -0800

    draw player, add movements

--- asteroids/main.py
+++ asteroids/main.py
@@ -3,28 +3,30 @@
 # throughout this file
 import pygame
 from constants import *
+from player import Player
 
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
+    # Time since last frame was drawn
     dt = 0
+    player = Player(PLAYER_START_X, PLAYER_START_Y)
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
 
-        update_screen(screen)
+        player.update(dt)
+
+        screen.fill("black")
+        player.draw(screen)
+        pygame.display.flip()
 
         # limit framerate to 60fps
         dt = clock.tick(60) / 1000
-
-
-def update_screen(screen):
-    screen.fill("black")
-    pygame.display.flip()
 
 
 if __name__ == "__main__":
