a7752f4577d91a01a7c415d0fa9a6b045b399393
Skylar Iolta <23390416+siolta@users.noreply.github.com>
2025-06-04 22:32:42 -0700

    open weather data fetcher, switch asteroids to uv

---
+++ mini-projects/weather_fetcher/main.py
@@ -0,0 +1,73 @@
+import os
+import requests
+import urllib.parse
+import argparse
+
+
+# check this isn't none before making calls to the api
+API_KEY = os.getenv("OW_API_KEY")
+BASE_URL = "http://api.openweathermap.org/"
+
+
+def get_location_args():
+    # add args for:
+    # zip code without input
+    # country code
+    # unit to return temps in
+
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument("zip", help="zip/postal code to request weather for")
+
+
+def get_location_via_input():
+    while True:
+        location = input("input location (zipcode): ")
+        if len(location) > 0:
+            break
+    return location
+
+
+def validate_location(location: str):
+    pass
+
+
+def get_coordinates(location: str):
+    geo_url = f"geo/1.0/zip?zip={location},US&appid={API_KEY}"
+    try:
+        r = requests.get(BASE_URL + geo_url)
+        r.raise_for_status()
+        location_data = r.json()
+    except requests.HTTPError as e:
+        SystemExit(e)
+
+    return location_data
+
+
+def request_weather_data(lat, lon):
+    data_url = f"data/2.5/weather?lat={lat}&lon={lon}&appid={API_KEY}&unit=metric"
+    try:
+        r = requests.get(BASE_URL + data_url)
+        r.raise_for_status()
+        weather_data = r.json()
+    except requests.HTTPError as e:
+        SystemExit(e)
+
+    return weather_data
+
+
+def display_forecast(forecast):
+    print(f"The weather is {forecast['weather'][0]['description']}")
+
+
+def main():
+    location = get_location_via_input()
+    location_data = get_coordinates(location)
+    # print(location_data)
+    forecast = request_weather_data(location_data["lat"], location_data["lon"])
+    # print(forecast)
+    display_forecast(forecast)
+
+
+if __name__ == "__main__":
+    main()
